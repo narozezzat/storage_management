c43db8d644d4af5b79463b436774cd7026ec734a
Naroz Ezzat <narozezzat10@gmail.com>
2024-11-09 00:19:31 +0200

    add layout for auth

--- app/page.tsx
+++ app/page.tsx
@@ -2,8 +2,8 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <div className="flex h-screen justify-center items-center">
-      <h1 className="text-3xl text-brand font-bold">StoreIt - The only storage solution you need.</h1>
+    <div className="flex-center h-screen">
+      <h1 className="h1">StoreIt - The only storage solution you need.</h1>
     </div>
   );
 }
